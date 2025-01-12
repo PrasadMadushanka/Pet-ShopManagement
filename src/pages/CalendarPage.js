19365b37a53b740adbdc9fb6ceb41856f87e9cb7
Prasad Madushanka <lahirumadushankap2000@gmail.com>
2025-01-12 18:47:07 +0530

    add new clender feature

---
+++ src/pages/CalendarPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CalendarScheduler from "../components/Calendar/CalendarScheduler";
+
+function CalendarPage() {
+  return (
+    <div>
+      <CalendarScheduler />
+    </div>
+  );
+}
+
+export default CalendarPage;
